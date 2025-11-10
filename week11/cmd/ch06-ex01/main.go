91dbbce3a3a827dda08273f8352342a9d552ee67
이예지 <jeje451212@gmail.com>
2025-11-10 14:27:58 +0900

    week11 slice v0.2

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -1,12 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var subjects []string
-	subjects = make([]string, 3)
-	subjects[0] = "Go"
-	subjects[2] = "Python"
+	// var subjects []string
+	// subjects = make([]string, 3)
+
+	// subjects := make([]string, 3)
+	// subjects[0] = "Go"
+	// subjects[2] = "Python"
+
+	subjects := []string{"Go", "", "Python"} // slice literal
 
 	for _, subject := range subjects {
 		fmt.Println(subject)

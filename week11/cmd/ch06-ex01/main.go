048a0061ed52b6f7898f85034ca9f06ae2646ab9
이예지 <jeje451212@gmail.com>
2025-11-10 14:35:19 +0900

    week11 slice v0.3

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -5,16 +5,14 @@ import (
 )
 
 func main() {
-	// var subjects []string
-	// subjects = make([]string, 3)
 
-	// subjects := make([]string, 3)
-	// subjects[0] = "Go"
-	// subjects[2] = "Python"
-
-	subjects := []string{"Go", "", "Python"} // slice literal
-
+	subjects := [4]string{"Go", "Javascript", "Python", "Linux"} // slice literal
+	subjectsSlice := subjects[1:3]                               // slicing
 	for _, subject := range subjects {
 		fmt.Println(subject)
+	}
+	fmt.Println("=============")
+	for i := 0; i < len(subjectsSlice); i++ {
+		fmt.Println(subjectsSlice[i])
 	}
 }
